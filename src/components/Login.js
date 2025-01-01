b98c4696a98e9357e3176ec826c44964e36db16b
Adeeba Fatima <adeebafatima1@gmail.com>
2025-01-01 19:43:02 +0530

    ep1_formValidation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Header from "./Header";
+import { validateData } from "../utils/validate";
 
 const Login = () => {
   const [isSignedIn, setIsSignedIn] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
 
   const toggleSignInSignUp = () => {
     setIsSignedIn(!isSignedIn);
+  };
+
+  const handleSignInSignUp = () => {
+    //Validate form data
+
+    const validationResult = validateData(
+      email.current.value,
+      password.current.value
+    );
+    
+    setErrorMessage(validationResult);
   };
   return (
     <div>
@@ -16,7 +32,10 @@ const Login = () => {
           src="https://assets.nflxext.com/ffe/siteui/vlv3/aa9edac4-a0e6-4f12-896e-32c518daec62/web/IN-en-20241223-TRIFECTA-perspective_1502c512-be5f-4f14-b21a-e3d75fe159ab_large.jpg"
         />
       </div>
-      <form className="absolute w-3/12 p-12 my-36 right-0 left-0 bg-black mx-auto text-white bg-opacity-85 rounded-lg">
+      <form
+        className="absolute w-3/12 p-12 my-36 right-0 left-0 bg-black mx-auto text-white bg-opacity-85 rounded-lg"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <h1 className="font-bold text-3xl py-4">
           {isSignedIn ? "Sign In" : "Sign Up"}
         </h1>
@@ -29,18 +48,24 @@ const Login = () => {
         )}
         <input
           type="text"
+          ref={email}
           placeholder="Email Address"
           className="p-3 my-3 w-full bg-gray-800"
         ></input>
         <input
           type="password"
+          ref={password}
           placeholder="Password"
           className="p-3 my-3 w-full  bg-gray-800"
         ></input>
-        <button className="py-3 my-3 w-full bg-red-600 rounded-lg">
+        <p className="font-bold tetx-lg py-3 text-red-700">{errorMessage}</p>
+        <button
+          className="py-3 my-3 w-full bg-red-600 rounded-lg"
+          onClick={handleSignInSignUp}
+        >
           {isSignedIn ? "Sign In" : "Sign Up"}
         </button>
-        <p onClick={toggleSignInSignUp}>
+        <p onClick={toggleSignInSignUp} className="cursor-pointer">
           {isSignedIn
             ? "New to Netflix? Sign Up now!"
             : "Already a user? Sign In now!"}
